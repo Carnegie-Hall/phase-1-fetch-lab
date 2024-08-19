fecf04bd9ea35d0fa6ca1ca4752126fe2d73fd7a
Carnegie Hall <genw.growth@gmail.com>
2024-08-19 13:37:46 -0400

    com

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((books) => renderBooks(books)); /* you took the json data here and turned it into books */ 
 }
-
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
@@ -11,7 +11,5 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  fetchBooks()});
